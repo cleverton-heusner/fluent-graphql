f3a044e90e8c6764430c8944537c34b0009ae6a3
cleverton-heusner <heusner.cleverton@gmail.com>
2024-10-02 15:43:12 -0300

    Junit updated and pom refactored.

--- src/test/java/GraphQlQueryParserTest.java
+++ src/test/java/GraphQlQueryParserTest.java
@@ -1,6 +1,6 @@
 import graphql.GraphQlQueryParser;
-import org.junit.Test;
 import fixtures.GetPostQuery;
+import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
