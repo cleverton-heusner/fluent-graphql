12682ac0bcb8650ba77b5a45792b85c2cae93d4c
cleverton-heusner <heusner.cleverton@gmail.com>
2024-10-16 10:32:23 -0300

    Tests refactored to pattern AAA.

--- src/test/java/GraphQlQueryParserTest.java
+++ src/test/java/GraphQlQueryParserTest.java
@@ -11,103 +11,136 @@ public class GraphQlQueryParserTest
     @Test
     public void when_oneLeafNodeSelected_then_queryWithOneNode()
     {
-        final GetPostQuery getPostQuery = new GetPostQuery().selectPostId().endSelection();
+        // Arrange
+        final String expectedQuery = "{\"query\":\"query{getPost{postId}}\"}";
+        final var getPostQuery = new GetPostQuery().selectPostId().endSelection();
+
+        // Act
         final String actualQuery = GraphQlQueryParser.parse(getPostQuery);
 
-        assertThat(actualQuery, is(equalTo("{\"query\":\"query{getPost{postId}}\"}")));
+        // Assert
+        assertThat(actualQuery, is(equalTo(expectedQuery)));
     }
 
     @Test
     public void when_moreThanOneLeafNodeSelected_then_queryWithMoreThanOneLeafNode()
     {
-        final GetPostQuery getPostQuery = new GetPostQuery().selectPostId()
+        // Arrange
+        final String expectedQuery = "{\"query\":\"query{getPost{postId postTitle postText}}\"}";
+        final var getPostQuery = new GetPostQuery().selectPostId()
                 .selectPostTitle()
                 .selectPostText()
                 .endSelection();
+
+        // Act
         final String actualQuery = GraphQlQueryParser.parse(getPostQuery);
 
-        assertThat(actualQuery, is(equalTo("{\"query\":\"query{getPost{postId postTitle postText}}\"}")));
+        // Assert
+        assertThat(actualQuery, is(equalTo(expectedQuery)));
     }
 
     @Test
     public void when_oneSubtreeWithOneLeafNodeAsChildSelected_then_queryWithOneSubtreeWithOneLeafNodeAsChild()
     {
-        final GetPostQuery getPostQuery = new GetPostQuery()
+        // Arrange
+        final String expectedQuery = "{\"query\":\"query{getPost{comments{commentsId}}}\"}";
+        final var getPostQuery = new GetPostQuery()
                 .fromComments()
                 .selectCommentsId()
                 .endCommentsSelection()
             .endSelection();
+
+        // Act
         final String actualQuery = GraphQlQueryParser.parse(getPostQuery);
 
-        assertThat(actualQuery, is(equalTo("{\"query\":\"query{getPost{comments{commentsId}}}\"}")));
+        // Assert
+        assertThat(actualQuery, is(equalTo(expectedQuery)));
     }
 
     @Test
     public void when_oneSubtreeWithOneSubtreeAsChildSelected_then_queryWithOneSubtreeWithOneSubtreeAsChild()
     {
-        final GetPostQuery getPostQuery = new GetPostQuery()
+        // Arrange
+        final String expectedQuery = "{\"query\":\"query{getPost{comments{author{authorId}}}}\"}";
+        final var getPostQuery = new GetPostQuery()
                 .fromComments()
                     .fromAuthor()
                     .selectAuthorId()
                     .endAuthorSelection()
                 .endCommentsSelection()
             .endSelection();
+
+        // Act
         final String actualQuery = GraphQlQueryParser.parse(getPostQuery);
 
-        assertThat(actualQuery, is(equalTo("{\"query\":\"query{getPost{comments{author{authorId}}}}\"}")));
+        // Assert
+        assertThat(actualQuery, is(equalTo(expectedQuery)));
     }
 
     @Test
     public void when_oneSubtreeWithMoreThanOneLeafNodeAsChildrenSelected_then_queryWithOneSubtreeWithMoreThanOneLeafNodeAsChildren()
     {
-        final GetPostQuery getPostQuery = new GetPostQuery()
+        // Arrange
+        final String expectedQuery = "{\"query\":\"query{getPost{comments{commentsId commentsText " +
+                "commentsDatePublished}}}\"}";
+        final var getPostQuery = new GetPostQuery()
                 .fromComments()
                 .selectCommentsId()
                 .selectCommentsText()
                 .selectCommentsDatePublished()
                 .endCommentsSelection()
                 .endSelection();
+
+        // Act
         final String actualQuery = GraphQlQueryParser.parse(getPostQuery);
 
-        assertThat(actualQuery, is(equalTo("{\"query\":\"query{getPost{comments{commentsId commentsText" +
-                " commentsDatePublished}}}\"}"))
-        );
+        // Assert
+        assertThat(actualQuery, is(equalTo(expectedQuery)));
     }
 
     @Test
     public void when_oneLeafNodeAndOneSubtreeSelected_then_queryWithOneLeafNodeAndOneSubtree()
     {
-        final GetPostQuery getPostQuery = new GetPostQuery()
+        // Arrange
+        final String expectedQuery = "{\"query\":\"query{getPost{postId comments{commentsId}}}\"}";
+        final var getPostQuery = new GetPostQuery()
                 .selectPostId()
                 .fromComments()
                 .selectCommentsId()
                 .endCommentsSelection()
                 .endSelection();
+
+        // Act
         final String actualQuery = GraphQlQueryParser.parse(getPostQuery);
 
-        assertThat(actualQuery, is(equalTo("{\"query\":\"query{getPost{postId comments{commentsId}}}\"}")));
+        // Assert
+        assertThat(actualQuery, is(equalTo(expectedQuery)));
     }
 
     @Test
     public void when_allFieldsSelected_then_queryWithAllFields()
     {
-        final GetPostQuery getPostQuery = new GetPostQuery()
-                .selectAllFields()
-                .endSelection();
+        // Arrange
+        final String expectedQuery = "{\"query\":\"query{getPost{postId postTitle postText postDatePublished postViews" +
+                " postLikes comments{commentsId commentsText commentsDatePublished author{authorId authorName" +
+                " authorEmail authorProfilePicture authorJoinedDate friends{friendsId friendsName friendsProfilePicture" +
+                " mutualFriends{mutualFriendsId mutualFriendsName}}}} popularity{likes dislikes}}}\"}";
+        final var getPostQuery = new GetPostQuery().selectAllFields().endSelection();
+
+        // Act
         final String actualQuery = GraphQlQueryParser.parse(getPostQuery);
 
-        assertThat(actualQuery, is(equalTo("{\"query\":\"query{getPost{postId postTitle postText" +
-                " postDatePublished postViews postLikes comments{commentsId commentsText commentsDatePublished" +
-                " author{authorId authorName authorEmail authorProfilePicture authorJoinedDate friends{friendsId" +
-                " friendsName friendsProfilePicture mutualFriends{mutualFriendsId mutualFriendsName}}}}" +
-                " popularity{likes dislikes}}}\"}"))
-        );
+        // Assert
+        assertThat(actualQuery, is(equalTo(expectedQuery)));
     }
 
     @Test
     public void when_lastLeafNodeSelected_then_queryWithLastLeafNode ()
     {
-        final GetPostQuery getPostQuery = new GetPostQuery()
+        // Arrange
+        final String expectedQuery = "{\"query\":\"query{getPost{comments{author{friends{mutualFriends{" +
+                "mutualFriendsId}}}}}}\"}";
+        final var getPostQuery = new GetPostQuery()
                 .fromComments()
                     .fromAuthor()
                         .fromFriends()
@@ -118,9 +151,11 @@ public class GraphQlQueryParserTest
                     .endAuthorSelection()
                 .endCommentsSelection()
             .endSelection();
+
+        // Act
         final String actualQuery = GraphQlQueryParser.parse(getPostQuery);
 
-        assertThat(actualQuery, is(equalTo("{\"query\":\"query{getPost{comments{author{friends{mutualFriends{" +
-                "mutualFriendsId}}}}}}\"}")));
+        // Assert
+        assertThat(actualQuery, is(equalTo(expectedQuery)));
     }
 }
